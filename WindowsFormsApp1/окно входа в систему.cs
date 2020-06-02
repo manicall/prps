81c17d33b9f86c213fa8809d03bfe9e29e812a48
max <maks04072000@mail.ru>
2020-06-02 10:13:44 +1000

    1

--- WindowsFormsApp1/окно входа в систему.cs
+++ WindowsFormsApp1/окно входа в систему.cs
@@ -1,22 +1,40 @@
 ﻿using System;
+using WindowsFormsApp1.Начальник_цеха;
+//using WindowsFormsApp1.Руководитель_комбината;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace WindowsFormsApp1
 {
-    static class Program
+    public partial class Form1 : Form
     {
-        /// <summary>
-        /// Главная точка входа для приложения.
-        /// </summary>
-        [STAThread]
-        static void Main()
+        public Form1()
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //new Form2().Show();
+            new Form4().Show();
+
+            Hide();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
